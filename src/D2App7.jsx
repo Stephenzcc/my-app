3245fa270a9cb94d51b21a8815b15ca4c0715ccc
Stephenzcc <401186943jrf@gmail.com>
2022-03-27 18:43:26 +0800

    1

--- src/D2App7.jsx
+++ src/D2App7.jsx
@@ -16,14 +16,14 @@ export default function D2App7() {
    * 使用setNum中写回调的方式setNum((num)=>num+1)不断使用新值覆盖旧值
    */
 
-  // const doSth = useCallback(() => {
-  //   setNum(num => num + 1)
-  // }, [])
-
-  // 函数中返回函数：高阶函数
-  const doSth = useMemo(() => {
-    return () => setNum(num => num + 1)
+  const doSth = useCallback(() => {
+    setNum(num => num + 1)
   }, [])
+
+  // // 函数中返回函数：高阶函数
+  // const doSth = useMemo(() => {
+  //   return () => setNum(num => num + 1)
+  // }, [])
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom";
 // import App from "./D2App9";
 import Router from "./router/index.jsx";
-// import { Provider } from "react-redux";
-// import store from "./store";
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.render(
   // <Provider store={store}>
